1b4ae049bad80b263578a089570722481d2e3aac
youngji <youngji.jang804@gmail.com>
2022-04-29 21:41:10 +0900

    e

--- youngji/5_longest_palindromic_substring.py
+++ youngji/5_longest_palindromic_substring.py
@@ -1,10 +1,9 @@
 
-s = "cbbd"
+s = "bb"
 
-s = list(s)
 l = len(s)
 
-maxi = 0
+# maxi = 0
 # for start in range(l) :
 #     for end in range(l) :
 #         words = s[start:end+1]
@@ -15,24 +14,24 @@ maxi = 0
 
 # print(''.join(result))
 
-start = 0
-end = 2
-
-if l == 1 :
+if l <= 1 :
     print(s)
-if s[:2] == s[1::-1] : 
-    result = s[:2]
-    maxi = 2
 
-while  1 :
-    if start > l or end > l :
-        break
-    words = s[start:end+1]
-    if words == words[::-1] :
-        if len(words) >= maxi :
-                result = words
-                maxi = len(words)
-                end += 1
-    else : start += 1
-    
-print(''.join(result))
+def check(start,end):
+    while start >= 0 and end < l :
+        word = s[start:end+1]
+        if word == word[::-1] :
+            start -= 1
+            end += 1
+        else : break
+    return s[start+1:end] #!!!!!!!!!!!!!!!!인덱스 진짜 헷갈린다. +1 -1
+
+result =''
+maxi = 0
+for i in range(l-1) :
+    a = check(i,i+1)
+    b = check(i,i+2)
+    result = max(result,a,b,key=len)
+
+
+print(result)
