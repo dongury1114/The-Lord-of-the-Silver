4126ec63ac04c9c25a8ea5ebfe327ff451a10023
youngji <youngji.jang804@gmail.com>
2022-05-01 01:27:52 +0900

    e

---
+++ 2022.05.01 #3/238_product_of_array_영지.py
@@ -0,0 +1,22 @@
+
+from operator import le
+
+
+nums = [-1,1,0,-3,3]
+
+right=[nums[0]]
+left=[nums[-1]]
+l = len(nums)
+for i in range(1,l-1) :
+    right.append(right[i-1]*nums[i])
+    left.append(left[i-1]*nums[l-1-i])
+
+print(right,left)
+result =[0]*l
+result[0] = left[-1]
+result[l-1] = right[-1]
+for i in range(1,l-1) :
+    result[i] = left[l-2-i] * right[i-1]
+
+
+print(result)
