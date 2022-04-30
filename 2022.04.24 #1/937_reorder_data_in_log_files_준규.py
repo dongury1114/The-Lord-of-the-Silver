dd3bdb8a9444e6efd280696b6835446afc032c69
donggyu <dongury1114@gmail.com>
2022-04-30 20:56:25 +0900

    new format

--- 2022.04.24 #1/937_reorder_data_in_log_files_준규.py
+++ 2022.04.24 #1/937_reorder_data_in_log_files_준규.py
@@ -1,15 +1,14 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        for i in range(len(s)//2):
-            s[i], s[len(s) -1 - i] = s[len(s) - 1 - i], s[i]
-        
-        # left, right = 0, len(s)-1
-        # while left < right:
-        #     s[left], s[right] = s[right], s[left]
-        #     left += 1
-        #     right -= 1
-        
-        # s.reverse()
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        from collections import deque
+        num = []
+        alp = []
+        for i in logs:
+            if i[-1].isdigit():
+                num.append(i)
+            else :
+                alp.append(i)
+        num.sort()
+        alp.sort(key= lambda x: ([i for i in x.split()[1:]], x.split()[0]))
+        return alp + num
+
