4222a56e8da6be1b5c23b264c4322afaae49dda5
youngji <youngji.jang804@gmail.com>
2022-04-30 22:48:47 +0900

    ee

--- youngji/42_장영지.py
+++ youngji/42_장영지.py
@@ -1,16 +1,17 @@
 height = [0,1,0,2,1,0,1,3,2,1,2,1]
 
 # 리스트 > 정수
+if not height :
+    print(0)
 
 start = 0
-end = 1
+end = len(height)-1
+
 water = 0
-
-# 처음에 상승할떄 처리 어떻게 할건지???
-
+ 
 while 1 :
-    if end == len(height) :
-        break
+    # if end == len(height) :;'
+        # break 
     if height[end-1] - height[end]  >= 0 : #하강
         end += 1
     else : #상승
