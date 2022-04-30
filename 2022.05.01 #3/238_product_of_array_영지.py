cc18967564a23a00fc76e92c52d9f57deb0283e1
youngji <youngji.jang804@gmail.com>
2022-05-01 01:29:28 +0900

    화잍이

--- 2022.05.01 #3/238_product_of_array_영지.py
+++ 2022.05.01 #3/238_product_of_array_영지.py
@@ -1,6 +1,4 @@
 
-from operator import le
-
 
 nums = [-1,1,0,-3,3]
 
@@ -11,12 +9,12 @@ for i in range(1,l-1) :
     right.append(right[i-1]*nums[i])
     left.append(left[i-1]*nums[l-1-i])
 
-print(right,left)
+# print(right,left)
 result =[0]*l
 result[0] = left[-1]
 result[l-1] = right[-1]
 for i in range(1,l-1) :
-    result[i] = left[l-2-i] * right[i-1]
+    result[i] = left[l-2-i] * right[i-1] #인덱스 겁나 헷갈림,,,, 감사합니다요
 
 
 print(result)
