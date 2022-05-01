c3dd73657ac7f99ae34928c0a03cd1fb3983d547
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-05-01 17:55:46 +0900

    20220501

--- test/2022.05.01/121_best-time_미정.py
+++ test/2022.05.01/121_best-time_미정.py
@@ -1,2 +1,11 @@
-
- 
+#거꾸로 해서. 큰 값을 계속 갱신
+class Solution(object):
+    def maxProfit(self, prices):
+        big = prices[-1]
+        ans = [0]
+        for i in range(len(prices)-1,-1,-1):
+            if prices[i] < big:
+                ans.append(big-prices[i])
+            else:
+                big = prices[i]
+        return (max(ans))
