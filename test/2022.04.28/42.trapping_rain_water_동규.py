89673c6d501e8d22d407562bb2077bcf30765f63
donggyu <dongury1114@gmail.com>
2022-05-01 14:01:27 +0900

    2022.05.01

--- test/2022.04.28/42.trapping_rain_water_동규.py
+++ test/2022.04.28/42.trapping_rain_water_동규.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-height = [0,1,0,2,1,0,1,3,2,1,2,1]
+height = [4,2,0,3,2,5]
 
 #최대 높이 만큼 돌면서
 #양끝이 양수면 카운트 시작 -> 투 포인터를 활용하면 될꺼같은데 무리
@@ -13,21 +13,21 @@ height = deque(height)
 count = 0 
 
 for _ in range(1, max(height)+1): # 첫줄에서는 문제 없이 동작 
-    for j in range(len(height)):
-        if height[j] <= 0:
+    for _ in range(len(height)):
+        if height[0] <= 0:
             height.popleft()
-            j += 1
-            break
+            # j += 1
+            
 
-        if height[a - 2] <= 0:
+        if height[len(height)-1] <= 0:
             height.pop()
-            a -= 1
-            break
-    count += height.count(0) # 이 부분도 수정해야함 0과 음수들로
-    
+            # a -= 1
+        #두번째 여기서 부터 바로 카운트로 넘어가네
+    for i in height:
+        if i <= 0:
+            count+= 1
     for i in range(len(height)):
         height[i] -= 1
-    
     print(height)
 
 
