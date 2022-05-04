2986fab3cdcbbae1b15385fe05b8d0a6d58354d5
youngji <youngji.jang804@gmail.com>
2022-05-04 22:37:05 +0900

    영지 제출..

---
+++ 2022.05.05#4/merge_two_영지.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        arr = []
+        while list1 :
+            arr.append(list1.val)
+            list1 = list1.next
+        while list2 :
+            arr.append(list2.val)
+            list2 = list2.next
+            
+        tmp = None
+        head = None
+
+        for val in sorted(arr):
+            if not tmp:
+                tmp = ListNode(val)
+                head = tmp
+            else:
+                tmp.next = ListNode(val)
+                tmp = tmp.next
+
+        return head
