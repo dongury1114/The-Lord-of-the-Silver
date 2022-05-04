d19764ac2fe147bfdf8a91c44bac68936d365e06
youngji <youngji.jang804@gmail.com>
2022-05-05 02:25:47 +0900

    추가

--- 2022.05.05#4/merge_two_영지.py
+++ 2022.05.05#4/merge_two_영지.py
@@ -7,7 +7,7 @@ class ListNode:
         self.val = val
         self.next = next
 
-
+#배열 떄려넣기
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         arr = []
@@ -30,3 +30,14 @@ class Solution:
                 tmp = tmp.next
 
         return head
+
+
+
+# 재귀함수로 풀기
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if (not list1) or (list2 and list1.val > list2.val) :
+            list1 ,list2 = list2, list1
+        if list1 :
+            list1.next = self.mergeTwoLists(list1.next,list2)
+        return list1
