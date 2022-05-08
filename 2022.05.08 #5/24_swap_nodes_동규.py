563ecc8fc4282c0ac2a3a7116d04c191968ccc77
donggyu <dongury1114@gmail.com>
2022-05-08 14:00:36 +0900

    2022.05.08

---
+++ 2022.05.08 #5/24_swap_nodes_동규.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head):
+        dummy = pre = ListNode(0)
+        pre.next = head
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = a.next
+            pre.next, a.next, b.next = b, b.next, a
+            pre = a
+        return dummy.next
