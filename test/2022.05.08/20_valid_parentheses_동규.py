56edf7183ca33ce55041588485c2d18584b731c9
donggyu <dongury1114@gmail.com>
2022-05-09 23:41:41 +0900

    2022.05.09

---
+++ test/2022.05.08/20_valid_parentheses_동규.py
@@ -0,0 +1,32 @@
+s = "()[]{}"
+
+
+# 갯수 체크
+# stack = list(s)
+
+
+# a_count = 0
+# b_count = 0
+# c_count = 0
+# d_count = 0
+# e_count = 0
+# f_count = 0
+
+# for i in stack:
+#     if i == "(":
+#         a_count += 1
+#     elif i == ")":
+#         b_count += 1
+#     elif i == "{":
+#         c_count += 1
+#     elif i == "}":
+#         d_count += 1
+#     elif i == "[":
+#         e_count += 1
+#     elif i == "]":
+#         f_count += 1
+
+# if (a_count == b_count) and (c_count == d_count) and (e_count == f_count):
+#     print("true")
+# else:
+#     print("false")
