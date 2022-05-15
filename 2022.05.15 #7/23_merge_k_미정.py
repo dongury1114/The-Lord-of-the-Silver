ad901ea84e06399e125006f8a2402879d2fb1054
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-05-15 17:30:48 +0900

    20220515

--- 2022.05.15 #7/23_merge_k_미정.py
+++ 2022.05.15 #7/23_merge_k_미정.py
@@ -14,12 +14,16 @@ class Solution(object):
         
         for i in range(len(lists)):
             if lists[i]:
+                #root값을 저장하는데, 중복된 값으로 인한 오류 방지를 위해 추가 인자를 넣는다 
                 heapq.heappush(heap, (lists[i].val, i, lists[i]))
         while heap:
-            node = heapq.heappop(heap)
+            node = heapq.heappop(heap)#가장 작은 노드의 연결 리스트부터 차례대로 나옴
             idx = node[1]
-            result.next = node[2]
+            result.next = node[2] #
             result = result.next
+            print(node, idx, result)
             if result.next:
                 heapq.heappush(heap,(result.next.val, idx, result.next))
         return root.next
+
+print(Solution().mergeKLists(lists))
