d0004f29b66fc31a8cfac3552fad3a8e5140c60d
youngji <youngji.jang804@gmail.com>
2022-05-18 14:41:17 +0900

    2018

---
+++ 2022.05.18 #8/347_top_k_영지.py
@@ -0,0 +1,21 @@
+
+from collections import Counter
+
+
+nums = [1,1,1,2,2,3]
+k = 2
+
+# c = dict()
+# for i in sorted(nums) :
+#     c[i] = nums.count(i)
+
+# c = sorted(c.items(), key=lambda x:x[1],reverse=True)[:k]
+
+# result = [x[0] for x in c]
+# print(result)
+
+c = Counter(nums)
+c = sorted(c.items(), key=lambda x:x[1],reverse=True)[:k]
+
+result = [x[0] for x in c]
+print(result)
