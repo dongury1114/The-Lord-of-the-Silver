f4d942bc94c02431213b50d3a42ed2f34b6d324d
youngji <youngji.jang804@gmail.com>
2022-05-18 13:58:13 +0900

    2

---
+++ 2022.05.18 #8/3_longest_substring_영지.py
@@ -0,0 +1,19 @@
+
+from collections import deque
+
+
+s = "bbbbb"
+
+stack = deque()
+count = 0
+p = None
+for i in s :
+    if stack and i in stack :
+        count = max(count,len(stack))
+        while 1:
+            p = stack.popleft()
+            if p == i : break
+    stack.append(i)
+
+count = max(count,len(stack))
+print(count)
