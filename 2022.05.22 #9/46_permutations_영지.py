129256528836508864d9c692e9818f0c9eb2d20f
youngji <youngji.jang804@gmail.com>
2022-05-22 17:02:12 +0900

    훗

--- 2022.05.22 #9/46_permutations_영지.py
+++ 2022.05.22 #9/46_permutations_영지.py
@@ -1,8 +1,25 @@
 import itertools
 from typing import List
 
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         return list(itertools.permutations(nums,len(nums)))
+
+nums = [1,2,3]
+
+result = []
+permute = []
+def dfs():
+    if len(permute) == len(nums) :
+        result.append(permute[:])
+        return
+    
+    for i in range(len(nums)) :
+        if nums[i] not in permute :
+            permute.append(nums[i])
+            dfs()
+            permute.pop()
+
+dfs()
+print(result)
 
