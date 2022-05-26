17dd5da45603edc1772a524549b0f25e2bac875b
donggyu <dongury1114@gmail.com>
2022-05-26 16:34:54 +0900

    2022.05.26

--- 2022.05.26 #10/78_subsets_승준.py
+++ 2022.05.26 #10/78_subsets_승준.py
@@ -1,25 +1,26 @@
 from typing import List
 
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         visited = [0] * n
         result = list()
-        
+
         def _DFS(level):
             if level == n:
                 tmp = list()
                 for i in range(n):
                     if visited[i] == 1:
                         tmp.append(nums[i])
-                    
+
                 result.append(tmp)
             else:
                 visited[level] = 1
                 _DFS(level + 1)
                 visited[level] = 0
                 _DFS(level + 1)
-        
+
         _DFS(0)
-        
+
         return result
