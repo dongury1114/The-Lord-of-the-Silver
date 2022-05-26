ce6d0400929e64105ef6d37539a7cf4fabb73ce9
youngji <youngji.jang804@gmail.com>
2022-05-26 22:30:59 +0900

    78

--- 2022.05.26 #10/78_subsets_영지.py
+++ 2022.05.26 #10/78_subsets_영지.py
@@ -1,1 +1,17 @@
-def 
+from typing import List
+
+nums = [1,2,3]
+
+result = [[]]
+def dfs(ans,index):
+    if ans :
+        result.append(ans)
+
+    for i in range(index,len(nums)) :
+        dfs(ans+[nums[i]],i+1)
+
+dfs([],0)
+
+print(result)
+
+
--- test/2022.05.22/39_combination_sum_영지.py
+++ test/2022.05.22/39_combination_sum_영지.py
@@ -2,23 +2,24 @@ candidates = [2,3,6,7]
 target = 7
 
 result = []
-ans = []
-def dfs():
+
+def dfs(index, ans):
     global target
-    if sum(ans) == target :
+    if ans and sum(ans) == target :
         result.append(ans[:])
         return 
-    elif sum(ans) > target :
+    elif ans and sum(ans) > target :
         return
 
-    for i in range(len(candidates)) :
-        ans.append(candidates[i])
-        dfs()
-        ans.pop()
+    for i in range(index, len(candidates)) :
+        dfs(i, ans + [candidates[i]])
+        # ans.pop()
     
-dfs()
-result = [sorted(i) for i in result]
-result = list(set([tuple(i) for i in result]))
+dfs(0,[])
+# result = [sorted(i) for i in result]
+# result = list(set([tuple(i) for i in result]))
 print(result)
 
-    
+
+# def dfs():
+#     pass
