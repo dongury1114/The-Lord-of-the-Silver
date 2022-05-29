e1086361380113de97baf1e2a92d732d7b97e9fd
박준규 <wnsrb003@naver.com>
2022-05-29 16:41:29 +0900

    jg commm

--- 2022.05.28 #11/543_diameter_of_jg.py
+++ 2022.05.28 #11/543_diameter_of_jg.py
@@ -5,6 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            nonlocal result
+
+            if node is None:
+                return 0
+            left = dfs(node.left)
+            right = dfs(node.right)
+            result = max(result, left+right)
+            return max(left, right) + 1
+        
+        result = 0
+        dfs(root)
+        return (result)
+
+class Solution:
     result: int = 0
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         def dfs(node):
