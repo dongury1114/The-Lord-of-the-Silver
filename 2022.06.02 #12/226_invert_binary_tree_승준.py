0bb716a37877d8e92bd1db2b521d4e2e6e6962ba
신승준 <jun@shinseungjun-ui-MacBookAir.local>
2022-06-01 00:41:59 +0900

    226_solved_SJ

---
+++ 2022.06.02 #12/226_invert_binary_tree_승준.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def _DFS(root):
+            if root:
+                root.left, root.right = root.right, root.left
+                _DFS(root.left)
+                _DFS(root.right)
+            else:
+                return
+        
+        _DFS(root)
+        
+        return root
+                
