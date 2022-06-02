ff04813d30cc737f62d6762df8f6302e1f792c81
donggyu <dongury1114@gmail.com>
2022-06-02 16:45:01 +0900

    2022.06.02

--- 2022.06.02 #12/297_serialize_deserialize_승준.py
+++ 2022.06.02 #12/297_serialize_deserialize_승준.py
@@ -1,14 +1,14 @@
 class Codec:
 
     def serialize(self, root: TreeNode) -> str:
-        
+
         q = collections.deque()
         q.append(root)
         result = list()
-        
+
         if root == None:
             return ''
-        
+
         while q:
             now = q.popleft()
             if now:
@@ -21,26 +21,26 @@ class Codec:
         print(' '.join(result))
         return ' '.join(result)
 
-    def deserialize(self, data):       
+    def deserialize(self, data):
         print(data)
         if len(data) == 0:
             return []
-        
+
         data = data.split()
         q = collections.deque()
-        
+
         for idx, value in enumerate(data):
             data[idx] = TreeNode(value)
-            
+
         for i in range(1, len(data) + 1):
             if i * 2 <= len(data):
                 data[i - 1].left = data[(i * 2) - 1]
-            
+
             if (i * 2) + 1 <= len(data):
                 data[i - 1].right = data[(i * 2 + 1) - 1]
-                
+
         return data[0]
-        
+
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
