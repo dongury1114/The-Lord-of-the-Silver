71b5b02f933b560f93ee1d5a2ad124f755a4404f
신승준 <jun@shinseungjun-ui-MacBookAir.local>
2022-06-04 12:57:21 +0900

    110_solved_SJ

---
+++ test/2022.06.02/110_balanced_binary_승준.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    result = True
+    
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        
+        def _DFS(node):
+            
+            if not node:
+                return 0
+            
+            else:
+                left = _DFS(node.left) + 1
+                right = _DFS(node.right) + 1
+                
+                if abs(left - right) > 1:
+                    self.result = False
+                
+                return max(left, right)
+                
+        _DFS(root)
+        
+        return self.result
+            
+                
+                
