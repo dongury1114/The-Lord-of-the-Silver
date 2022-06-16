2a26640ce8389c537b56d0a3cca818f2b88d8a42
박준규 <wnsrb003@naver.com>
2022-06-16 15:08:19 +0900

    re

--- 2022.06.15 #16/75_sort_color_jg.py
+++ 2022.06.15 #16/75_sort_color_jg.py
@@ -1,14 +1,61 @@
 class Solution:
+    def q(self, nums):
+        if len(nums) <= 1:
+            return nums
+        pivot = nums[len(nums)//2]
+        left, same, right = [], [], []
+        for n in nums:
+            if n < pivot:
+                left.append(n)
+            elif n > pivot:
+                right.append(n)
+            else:
+                same.append(n)
+        return self.q(left) + same + self.q(right)
+    
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = 1
-        while i < len(nums):
-            j = i
-            while j > 0:
-                if nums[j-1] > nums[j]:
-                    nums[j-1], nums[j] = nums[j], nums[j-1]
-                j -= 1
-            i += 1
-        return (nums)
+        #삽입
+        # i = 1
+        # while i < len(nums):
+        #     j = i
+        #     while j > 0:
+        #         if nums[j-1] > nums[j]:
+        #             nums[j-1], nums[j] = nums[j], nums[j-1]
+        #         j -= 1
+        #     i += 1
+        #퀵
+        result = self.q(nums)
+        llist = [num for num in result]
+        print(llist)
+        return llist# class Solution:
+def sortColors(nums):
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    #삽입
+    # i = 1
+    # while i < len(nums):
+    #     j = i
+    #     while j > 0:
+    #         if nums[j-1] > nums[j]:
+    #             nums[j-1], nums[j] = nums[j], nums[j-1]
+    #         j -= 1
+    #     i += 1
+    #퀵
+    if len(nums) <= 1:
+        return nums
+    pivot = nums[len(nums)//2]
+    left, same, right = [], [], []
+    for n in nums:
+        if n < pivot:
+            left.append(n)
+        elif n > pivot:
+            right.append(n)
+        else:
+            same.append(n)
+    return sortColors(left) + same + sortColors(right)
+
+print(sortColors([2,0,2,1,1,0]))
