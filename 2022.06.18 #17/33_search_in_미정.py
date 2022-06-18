9a83a96d27e2a3c1e40441ae6b7e3013bdda26d7
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-06-18 11:32:19 +0900

    20220618

---
+++ 2022.06.18 #17/33_search_in_미정.py
@@ -0,0 +1,12 @@
+
+#간단 풀이:
+# class Solution(object):
+    # def search(self, nums, target):
+    #     if target in nums:
+    #         return nums.index(target)
+    #     else:
+    #         return -1
+
+
+#Runtime: 30 ms, faster than 78.79% of Python online submissions for Search in Rotated Sorted Array.
+# Memory Usage: 13.7 MB, less than 65.39% of Python online submissions for Search in Rotated Sorted Array.
