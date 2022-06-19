331efbf41c236f025abae0dcc6edbc13619aedfa
donggyu <dongury1114@gmail.com>
2022-06-19 23:48:43 +0900

    2022.06.19

--- test/prac.py
+++
@@ -1,8 +0,0 @@
-numbers = [2, 3, 4]
-target = 6
-
-for i in range(len(numbers)):
-    for j in range(i, len(numbers)):
-        if numbers[i]+numbers[j] == target:
-            ans = [i+1, j+1]
-print(ans)
