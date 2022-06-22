1f81821eaf59dd2c3d05a89a32fbe46b74b6b4a7
youngji <youngji.jang804@gmail.com>
2022-06-23 01:43:34 +0900

    h

--- 2022.06.15 #16/179_largest_number_영지.py
+++ 2022.06.15 #16/179_largest_number_영지.py
@@ -4,7 +4,7 @@ from itertools import permutations
 nums = [3,30,34,5,9,90,97]
 nums = list(map(str,nums))
 
-s_num = sorted(nums, key= lambda x:int(x)*10)[::-1]
+s_num = sorted(nums, key= lambda x:x*2)[::-1]
 
 print(s_num)
 
